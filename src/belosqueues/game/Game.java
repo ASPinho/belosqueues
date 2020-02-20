576b9b2b4b81091879dfd4d23bd4225870516207
DavidBorges <borges.david6@gmail.com>
2020-02-20 22:49:53 +0000

    The enemy finds the path

--- src/belosqueues/game/Game.java
+++ src/belosqueues/game/Game.java
@@ -2,6 +2,8 @@ package belosqueues.game;
 
 import belosqueues.game.Classes.Player;
 import belosqueues.game.Classes.PlayerFactory;
+import belosqueues.game.entities.Enemy;
+import belosqueues.game.entities.EnemyFactory;
 import belosqueues.game.stages.Stage;
 import belosqueues.game.stages.Tutorial;
 
@@ -14,11 +16,27 @@ public class Game {
 
     private Stage tutorial;
     private Player player;
+    private Enemy enemy;
 
 
-    public Game(Player.PlayerClasses playerClasses){
+    public Game(Player.PlayerClasses playerClasses) {
         tutorialInit();
         player = PlayerFactory.newPlayer(playerClasses);
+        enemy = EnemyFactory.newEnemy();
+
+        gameFlow();
+    }
+
+    public void gameFlow(){
+
+        while(!player.getPos().equals(enemy.getPos())) {
+            moveEnemy();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void movePlayer(KeyboardController.MoveDirections direction){
@@ -50,6 +68,49 @@ public class Game {
                 break;
         }
 
+    }
+
+    public void moveEnemy(){
+
+        int[] distance = new int[4];
+
+        int difCol = player.getPos().getCol() - enemy.getPos().getCol();
+        int difRow = player.getPos().getRow() - enemy.getPos().getRow();
+
+        //move up
+        distance[0] = (int) Math.sqrt(Math.pow(difCol,2) + Math.pow((difRow - 1),2));
+
+        //move down
+        distance[1] = (int) Math.sqrt(Math.pow(difCol,2) + Math.pow((difRow + 1),2));
+
+        //move left
+        distance[2] = (int) Math.sqrt(Math.pow((difCol-1),2) + Math.pow(difRow,2));
+
+        //move right
+        distance[3] = (int) Math.sqrt(Math.pow((difCol+1),2) + Math.pow(difRow,2));
+
+        int min = (int) Math.sqrt(Math.pow(difCol,2) - Math.pow(difRow,2));
+
+        for (int i = 1; i <distance.length ; i++) {
+            if (distance[i] > min){
+                min = distance[i];
+            }
+        }
+        if(min == distance[0]) {
+            enemy.moveUp();
+            return;
+        }
+        if(min == distance[1]) {
+            enemy.moveDown();
+            return;
+        }
+        if(min == distance[2]) {
+            enemy.moveLeft();
+            return;
+        }
+        if(min == distance[3]) {
+            enemy.moveRight();
+        }
     }
 
     public void tutorialInit(){
--- src/belosqueues/game/entities/Enemy.java
+++ src/belosqueues/game/entities/Enemy.java
@@ -3,6 +3,7 @@ package belosqueues.game.entities;
 
 import belosqueues.game.Game;
 import belosqueues.game.components.Position;
+import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public abstract class Enemy {
@@ -20,13 +21,38 @@ public abstract class Enemy {
         this.stamina = stamina;
         this.strength = strength;
         this.intellect = intellect;
-        pos = new Position(10,20);
+        pos = new Position(10,4);
         enemy = new Rectangle(Game.PADDING + pos.getCol() * Game.CELLSIZE, Game.PADDING + pos.getRow() * Game.CELLSIZE, Game.CELLSIZE, Game.CELLSIZE);
+        enemy.setColor(Color.RED);
         enemy.fill();
     }
 
     public void enemyAttack(){
 
+    }
+
+    public void moveUp(){
+        pos.moveRow(-1);
+        enemy.translate(0, -Game.CELLSIZE);
+    }
+
+    public void moveDown(){
+        pos.moveRow(1);
+        enemy.translate(0, Game.CELLSIZE);
+    }
+
+    public void moveLeft(){
+        pos.moveCol(-1);
+        enemy.translate( -Game.CELLSIZE, 0);
+    }
+
+    public void moveRight(){
+        pos.moveCol(1);
+        enemy.translate(Game.CELLSIZE, 0);
+    }
+
+    public Position getPos() {
+        return pos;
     }
 
     public enum EnemyClasses {
