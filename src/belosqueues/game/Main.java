b8818611fe2675333cd9ba5ebfa66cb82781e7e8
André Pinho <andreseguradopinho@gmail.com>
2020-03-01 18:55:11 +0000

    First Engine outputs

--- src/belosqueues/game/Main.java
+++ src/belosqueues/game/Main.java
@@ -4,17 +4,39 @@ import belosqueues.game.player.Player;
 import belosqueues.game.player.PlayerFactory;
 import belosqueues.game.enemies.EnemyFactory;
 import belosqueues.game.stages.Arena;
+import static belosqueues.game.Engine.*;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Engine.init();
+
+        String pickedClass = scanner.nextLine().toLowerCase();
+
+        switch (pickedClass){
+
+            case "warlock":
+                System.out.println("you've picked the warlock class");
+                break;
+
+            case "warrior":
+                System.out.println("you've picked the warrior class");
+                break;
+
+            default:
+                System.out.println("not a valid class");
+
+        }
+
         //Game master = new Game(Player.PlayerClasses.WARLOCK);
 
         //KeyboardController kb = new KeyboardController(master);
 
-        Arena arena = new Arena(PlayerFactory.newPlayer(Player.PlayerClasses.WARRIOR), EnemyFactory.newEnemy());
-        arena.startBattle();
+        //Arena arena = new Arena(PlayerFactory.newPlayer(Player.PlayerClasses.WARRIOR), EnemyFactory.newEnemy());
+        //arena.startBattle();
 
     }
 
